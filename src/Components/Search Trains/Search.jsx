90ed25b32c7d0e811ce8bb2479dd5047e142ab48
Sreejit <sreesen2003@gmail.com>
2023-07-21 18:43:22 +0530

    Changes method of fetching API

--- src/Components/Search Trains/Search.jsx
+++ src/Components/Search Trains/Search.jsx
@@ -1,17 +1,16 @@
 import { Button } from "@chakra-ui/react";
 import React from "react";
 import InputField from "./InputField";
-import DatePick from "./DatePick";
-import TrainDetail from "./TrainDetail";
 import SourceDestinationHeader from "./SourceDestinationHeader";
-import { trainData } from "./trainData";
-import { process } from "../../../env";
+import TrainDetail from "./TrainDetail";
+import DatePick from "./DatePick";
+import { process } from '../../../env'
 
 export default function Search() {
   const [sourceStnCode, setSourceStnCode] = React.useState("");
   const [desStnCode, setDesStnCode] = React.useState("");
   const [date, setDate] = React.useState("");
-  const [trainList, setTrainList] = React.useState("ls");
+  const [trainList, setTrainList] = React.useState([]);
   console.log(trainList);
 
   async function getTrains() {
@@ -24,21 +23,26 @@ export default function Search() {
       },
     };
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      setTrainList(result.data);
-    } catch (error) {
-      console.error(error);
-    }
+    // try {
+    //   const response = await fetch(url, options);
+    //   const result = await response.json();
+    //   setTrainList(result.data);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    await fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => setTrainList(data.data));
   }
 
-  const trains = trainData.map((item) => {
+  const train = trainList.map((item) => {
     return (
       <>
         <SourceDestinationHeader
           source={item.from_station_name}
           destination={item.to_station_name}
+          key={item.train_number}
         />
         <TrainDetail
           type={item.train_type}
@@ -52,8 +56,8 @@ export default function Search() {
       </>
     );
   });
-  return trainList ? (
-    { trains }
+  return trainList.length ? (
+    { train }
   ) : (
     <div className="bg-[#EEEEEE] mx-auto w-[80%] rounded-xl mt-10 h-[320px]">
       <p className="font-lexend flex justify-center items-center pt-4">
